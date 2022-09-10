baa3b7ae9556bc920289de7a9bf7c405aeafe687
oldnew194 <90535867+oldnew194@users.noreply.github.com>
2022-09-10 15:34:43 +0900

    いいねとブックマークボタンのAjax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    store = Store.find(params[:store_id])
-    current_user.favorite(store)
-    redirect_back fallback_location: root_path, success: t('.success')
+    @store = Store.find(params[:store_id])
+    current_user.favorite(@store)
+    #redirect_back fallback_location: root_path, success: t('.success')
   end
 
   def destroy
     #binding.pry
-    store = current_user.favorites.find(params[:id]).store
-    current_user.unfavorite(store)
-    redirect_back fallback_location: root_path, success: t('.success')
+    @store = current_user.favorites.find(params[:id]).store
+    current_user.unfavorite(@store)
+    #redirect_back fallback_location: root_path, success: t('.success')
   end
 end
